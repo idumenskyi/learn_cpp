be8de2991ed1d27d7e82c66c020bec1e2914e6e7
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-05-18 22:02:48 +0300

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //int a;
+    //a = 5;
+    //int name var; wrong declaration variable, using whitespase in name of variable
+    //int a = 7;
+    //cout << a << endl;
+    //a = 108;
+    //cout << a << endl;
+    int Age, a, b; // int Age = 26, a = 2, b = 5;
+    double e = 2.72;
+    char s = 'f';
+    bool tr = true, fl = false;
+    Age = 26;
+    a = 2;
+    b = 5;
+    cout << Age << " " << a << " " << b << " " << e << " " << s << " " << tr << " " << fl << endl;
+    return 0;
+}
