f072bb3b4077a551c22376dccf937dac90202f0f
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-05-18 23:39:28 +0300

    Create statements_arithmetic.cpp

---
+++ statements_arithmetic.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //int num = 8;
+    //int num = -8;
+    //cout << num << "\n";
+    //num = -num;
+    //cout << num << endl;
+    int num1 = 90, num2 = 8, result;
+    result = num1 + num2;
+    cout << result << "\n";
+    result = num1 * num2;
+    cout << result << "\n";
+    result = num1 - num2;
+    cout << result << "\n";
+    result = num1 / num2;
+    cout << result << "\n";
+    result = num1 % num2;
+    cout << result << "\n";
+    result = num1 <= num2;
+    cout << result << "\n";
+    result = num1 >= num2;
+    cout << result << "\n";
+    result = num1 != num2;
+    cout << result << "\n";
+    result = num1 == num2;
+    cout << result << "\n";
+
+
+
+
+    return 0;
+}
