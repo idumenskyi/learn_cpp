81d68758a8b57c81c799123a1412d23f58cb7275
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-05-18 22:30:05 +0300

    Create constants.cpp

---
+++ constants.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int COUNT_DAYS_IN_WEEK = 7;
+    const char NEW_LINE = '\n';
+    const char TAB = '\t';
+    //cout << COUNT_DAYS_IN_WEEK << "\n";
+    //cout << COUNT_DAYS_IN_WEEK << NEW_LINE;
+    cout << COUNT_DAYS_IN_WEEK << TAB;
+
+
+    return 0;
+}
