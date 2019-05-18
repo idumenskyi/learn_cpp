3cac8e9471f1917b8257f406174eaba07aae8fe1
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-05-18 23:17:22 +0300

    Create input_data.cpp

---
+++ input_data.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    //cout << "Please type a number 1: ";
+    //cin >> num1;
+    //cout << "Please type a number 2: ";
+    //cin >> num2;
+    cout << "Please type two numbers: ";
+    cin >> num1 >> num2;
+    cout << "You typed number 1: " << num1 << "\n" << "You typed number 2: " << num2 << "\n";
+
+    return 0;
+}
