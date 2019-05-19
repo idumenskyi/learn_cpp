a0910f5fe29f439ef72c074c391165bc8e6ccd96
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-05-19 18:02:26 +0300

    Create short_arithmetic_form.cpp

---
+++ short_arithmetic_form.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num = 1;
+    cout << num << endl;
+    num = num + 10;
+    cout << num << endl;
+    num = 1;
+    cout << num << endl;
+    num += 10;
+    cout << num << endl;
+    num *= 2;
+    cout << num << endl;
+    num -= 1;
+    cout << num << endl;
+    num /= 2;
+    cout << num << endl;
+    return 0;
+}
