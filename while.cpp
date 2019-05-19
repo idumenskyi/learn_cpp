f0f95d456ab06212cc74aaded93b2ac90d964607
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-05-19 21:21:56 +0300

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int val = 0;
+    while (val < 10)
+    {
+        cout << "Variable val = " << val << endl;
+        val++;
+        //break;
+    }
+
+    return 0;
+}
