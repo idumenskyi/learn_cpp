66b735f6d2bf5fe400a55809538895d594cbe941
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-06-02 13:07:25 +0300

    Create arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[4];
+	arr[0] = 8;
+	arr[1] = 3;
+	arr[2] = 2;
+	arr[3] = 29;
+	cout << "array: " << arr[0] << endl;
+	return 0;
+}
