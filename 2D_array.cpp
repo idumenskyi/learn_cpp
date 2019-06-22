9c06b8f963c673cdfbc92787b59bafa9cab65d1d
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-06-22 22:04:17 +0300

    Create 2D_array.cpp

---
+++ 2D_array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int ROW = 3;
+    const int COL = 4;
+
+    int arr[ROW][COL]
+    {
+        {2, 4, 66, 1},
+        {4344, 3, 6, 1},
+        {58, 9, 90}
+    };
+
+    return 0;
+}
