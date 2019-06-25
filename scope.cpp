e9a5522a7cce039dc406eb4961a47fb92422ea87
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-06-25 19:24:21 +0300

    Create scope.cpp

---
+++ scope.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// Scope, Global and local variables
+int l; // global variable
+int main()
+{
+    cout << l << endl; // output 0
+    /*if(true)
+    {
+        int d;//local variable
+        d = 0;
+    }*/
+    return 0;
+}
