17ae07b64bd0f73a1be38d36c7159908474258d1
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-07-13 23:48:31 +0300

    Update function_constant_arguments_constant_pointer.cpp

--- function_constant_arguments_constant_pointer.cpp
+++ function_constant_arguments_constant_pointer.cpp
@@ -11,7 +11,7 @@ void ShowArray(const int* const arr, const int size)
 {
     for(int i = 0; i < size; i++)
     {
-        cout << arr << "\t";
+        cout << arr[i] << "\t";
     }
     cout << endl;
 }
