9cca16ccf991df400764aa08459659b1dc2eb5fa
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-07-16 22:06:25 +0300

    Create preprocessor_directive_define_macros.cpp

---
+++ preprocessor_directive_define_macros.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define E 2.72
+#define tab "\t"
+#define start {
+#define fin }
+using namespace std;
+
+int main()
+{
+    cout << "Euler's number" << tab << E << endl;
+    for(int i = 0; i < 5; i++)
+    start
+        cout << "Euler's number" << tab << i << tab << E << endl;
+    fin
+    return 0;
+}
