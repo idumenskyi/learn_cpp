e65ad3fc13080b15fdaebad9ce7e3a59fb28f405
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-08-17 22:28:37 +0300

    Create friendly_class_method.cpp

---
+++ friendly_class_method.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Human;
+class Apple;
+class Human
+{
+public:
+    void TakeApple(Apple &apple);
+
+};
+class Apple
+{
+public:
+    Apple(int weight, string color)
+    {
+        this->weight = weight;
+        this->color = color;
+    }
+private:
+    int weight;
+    string color;
+    friend void Human::TakeApple(Apple &apple);
+};
+
+int main()
+{
+    Apple apple(120, "Green");
+    Human human;
+    human.TakeApple(apple);
+    return 0;
+}
+void Human::TakeApple(Apple &apple)
+{
+        cout << "TakeApple " << " weight = " << apple.weight << " color = " << apple.color << endl;
+}
